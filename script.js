32d941b6245ec5bbae5c786beacc038613631d83
gustavwiese <gusse12345@gmail.com>
2023-04-12 11:21:05 +0200

    ændret højde på første søjle

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-"use strict"
+"use strict";
+
+window.addEventListener("load", start);
+
+function start() {
+  console.log("JavaScript Kører!");
+
+  const bars = document.querySelectorAll(".bar");
+  const firstBar = bars[0];
+  const height = 50
+
+  firstBar.style.height = `${height}px`
+
+}
