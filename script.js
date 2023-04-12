1ee6b0bf46d235d58060a32176b6119f54473ed1
gustavwiese <gusse12345@gmail.com>
2023-04-12 11:47:11 +0200

    displayData færdig

--- script.js
+++ script.js
@@ -2,14 +2,23 @@
 
 window.addEventListener("load", start);
 
-const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 1, 2, 3, 4, 5, 6, 7, 8]
+const data = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8,
+  7, 6, 5, 4, 3, 2, 1, 1,
+];
 
 function start() {
   console.log("JavaScript Kører!");
+  displayData();
+}
 
+function displayData() {
   const bars = document.querySelectorAll(".bar");
-  const firstBar = bars[0];
-  const height = data[0] / 32 * 100;
 
-  firstBar.style.height = `${height}px`;
+  for (let i = 0; i < 40; i++) {
+    const currentBar = bars[i];
+    const height = (data[i] / 32) * 100;
+
+    currentBar.style.height = `${height}px`;
+  }
 }
